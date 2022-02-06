44e04f66f97d7a6be24911a09a015b95247eb035
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-02-06 13:34:13 -0800

    get api

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const { v4: uuidv4 } = require('uuid');;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
