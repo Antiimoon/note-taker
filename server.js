19ac821e6c49704d24fc464f3f8673fe8098cccf
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-02-06 19:49:55 -0800

    delete notes

--- server.js
+++ server.js
@@ -13,3 +13,19 @@ app.use(express.static("public"));
 app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"))
 });
+
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuidv4(); 
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+});
