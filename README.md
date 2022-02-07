8e1b73241079c5c59a562c3191c07d5f9b45bc95
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-02-07 13:04:53 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # note-taker
+
+# Installation
+* node.js
+* express
+* uuid
+* heroku deployment
+
+# Usage 
+A simple note taker application used to track and delete notes taken by the user.
+
+# Contribution
+Made by Antoinette Munoz.
+
+# Link
+https://note-taker0000.herokuapp.com/notes
--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("/public"));
 
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"))
+    res.sendFile(path.join(__dirname, "./db/db.json"))
 });
 
 app.post("/api/notes", (req, res) => {
